7810e5e1ff46fffe25979062bf3a12925d323e35
jakebarnes <jake.barnes@digital.hmrc.gov.uk>
2021-04-28 13:22:45 +0100

    [JB] compile error in tests

--- app/pp/model/TaxTypes.scala
+++ app/pp/model/TaxTypes.scala
@@ -130,6 +130,8 @@ object TaxTypes extends Enum[TaxType] {
   case object `climateChangeLevy` extends TaxType
 
   case object cds extends TaxType
+  case object ChildBenefitsRepayments extends TaxType
+  case object childbenefitsrepayments extends TaxType
 
   override def values: immutable.IndexedSeq[TaxType] = findValues
 }
