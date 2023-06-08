57f22b15d9a10d289a4673fecb1e828f36b9dbf9
Wojciech Gradzki <40370552+gitwojciech@users.noreply.github.com>
2023-06-08 11:32:53 +0200

    [WG][OPS-8686] Enable Coverage at 80% (#72)
    * [WG][WG/OPS-8686] Enable Coverage at 80%
    
    * [WG][WG/OPS-8686] Enable Coverage at 80%
    
    ---------
    
    Co-authored-by: wojciech.gradzki <gitwojciech@users.noreply.github.com>

--- project/ScoverageSettings.scala
+++ project/ScoverageSettings.scala
@@ -1,26 +1,38 @@
-import sbt.Keys.parallelExecution
 import sbt._
 import scoverage.ScoverageKeys
 
 object ScoverageSettings {
-  def apply() = Seq( // Semicolon-separated list of regexes matching classes to exclude
-    ScoverageKeys.coverageExcludedPackages := "<empty>;Reverse.*;.*(config|views.*);.*(AuthService|BuildInfo|Routes).*",
-    ScoverageKeys.coverageExcludedFiles := Seq(
-      "" +
-        "<empty>",
-      "Reverse.*",
-      ".*models.*",
-      ".*repositories.*",
-      ".*BuildInfo.*",
-      ".*javascript.*",
-      ".*Routes.*",
-      ".*GuiceInjector",
-      ".*DateTimeQueryStringBinder.*", // better covered via wiremock/E2E integration tests
-      ".*Test.*"
-    ).mkString(";"),
-    ScoverageKeys.coverageMinimumStmtTotal := 80, //should be a lot higher but we are where we are
-    ScoverageKeys.coverageFailOnMinimum := true,
-    ScoverageKeys.coverageHighlighting := true,
-    Test / parallelExecution := false
-  )
+  lazy val scoverageSettings: Seq[Def.SettingsDefinition] = {
+
+    val excludedFiles = Seq(
+      """<empty>""",
+      """Reverse.*""",
+      """.*models.*""",
+      """.*.template.*""",
+      """.*components.*""",
+      """.*Routes.*""",
+      """.*Link.*""",
+      """.*TestOnly.scala""",
+      """.*JourneyLogger.scala"""
+    ).mkString("", ";", ";")
+
+    val excludedPackages = Seq(
+      """.*.Reverse.*""",
+      """app.Routes.*""",
+      """prod.*""",
+      """.*.javascript.*""",
+      """testOnly.*""",
+      """websocket.*""",
+      """.*viewmodels.govuk""",
+      """.*Reverse.*"""
+    ).mkString("", ";", ";")
+
+    Seq(
+      ScoverageKeys.coverageExcludedPackages := excludedPackages,
+      ScoverageKeys.coverageExcludedFiles := excludedFiles,
+      ScoverageKeys.coverageMinimumStmtTotal := 80,
+      ScoverageKeys.coverageFailOnMinimum := true,
+      ScoverageKeys.coverageHighlighting := true
+    )
+  }
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -14,7 +14,7 @@ addSbtPlugin("uk.gov.hmrc"       %  "sbt-distributables"     % "2.2.0")
 
 addSbtPlugin("com.typesafe.play" %  "sbt-plugin"             % "2.8.19")
 addSbtPlugin("org.wartremover"   %  "sbt-wartremover"        % "3.0.7")
-addSbtPlugin("org.scoverage"     %  "sbt-scoverage"          % "2.0.5")
+addSbtPlugin("org.scoverage"     %  "sbt-scoverage"          % "2.0.6")
 addSbtPlugin("org.scalariform"   %  "sbt-scalariform"        % "1.8.3")
 addSbtPlugin("org.scalastyle"    %% "scalastyle-sbt-plugin"  % "1.0.0")
 addSbtPlugin("com.timushev.sbt"  %  "sbt-updates"            % "0.6.3")
